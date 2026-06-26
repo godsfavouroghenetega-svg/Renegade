23b25b9843de12c4250d67bd09046e090ff2b345
God'sfavour <godsfavouroghenetega@gmail.com>
2026-06-26 15:39:10 +0100

    trial 3

---
+++ script.js
@@ -0,0 +1,15 @@
+const fruits = ['Apple', 'Banana', 'Cherry'];
+let x = '';
+myfavorite: {
+  x = fruits[0];
+  break myfavorite;
+  x = fruits[1];
+  x = fruits[2];
+}
+console.log(x);
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+
+console.log(length)
+
